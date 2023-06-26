a9c623e59b87457ce726badcd5410bb6b38e3b8b
Lucca-Lopes <luccavlopes@hotmail.com>
2023-06-26 19:16:02 -0300

    feat: scriptables roupas iniciados

---
+++ Jam/Assets/_Scripts/RoupaBehavior.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoupaBehavior : MonoBehaviour
+{
+    public PecaDeRoupa roupaItem;
+
+    private void Start()
+    {
+        //var sr = gameObject.GetComponent<SpriteRenderer>();
+        //sr.sprite = roupaItem.sprite;
+    }
+}
